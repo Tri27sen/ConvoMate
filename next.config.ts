c72ef89297a9bced439442d6150bb12e388d18f7
Tri27sen <trishasengupta27@gmail.com>
2025-01-11 23:45:41 +0530

    Resolve merge conflict in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-=======
-  /* config options here */
-   /* config options here */
->>>>>>> 5e61a544b54be0b175709c38f0846ea05a33444a
   eslint: {
     ignoreDuringBuilds: true,
   },
